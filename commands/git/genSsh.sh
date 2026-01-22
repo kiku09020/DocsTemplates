da1becd275cf52cbca9415deced9fcaac11d6888
KK09020 <kiku09020@gmail.com>
2026-01-22 22:00:02 +0900

    gitコマンド追加

---
+++ commands/git/genSsh.sh
@@ -0,0 +1,2 @@
+ssh-keygen -t ed25519 -C "kiku09020@gmail.com"
+clip < ~/.ssh/id_ed25519.pub
---
+++ commands/git/userConfig.sh
@@ -0,0 +1,4 @@
+git config --global user.name "KK09020"
+git config --global user.email "kiku09020@gmail.com"
+git config --global core.editor "code"
+git config --global -l
